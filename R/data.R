8650ab3d2965b48898b6966eb567bab3bac440f0
Juan Carrasco <jgcarrascob@ine.gob.cl>
2023-10-22 18:59:08 -0300

    package commit

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Accidents in 2013
+#'
+#' @source the US National Highway Traffic Safety Administration's Fatality Analysis Reporting System,
+#' which is a nationwide census providing the American public yearly data regarding fatal injuries suffered
+#' in motor vehicle traffic crashes.
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{STATE}{A value that indicates the State}
+#'  \item{ST_CASE}{ID of the case}
+#'  \item{VE_TOTAL}{The VE total}
+#' }
+#' @examples
+#' \dontrun{
+#'  accident_2013
+#' }
+"accident_2013"
---
+++ data-raw/accident-2013.R
@@ -0,0 +1,4 @@
+## code to prepare `accident 2013` dataset goes here
+
+accident_2013  <- read.csv("C:/Users/juan_/Downloads/data/data/accident_2013.csv")[1:10,1:3]
+usethis::use_data(accident_2013, overwrite = TRUE)
