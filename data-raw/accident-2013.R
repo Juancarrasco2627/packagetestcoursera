210348df175af7e3ffb4eb4b1c39df62b3467d93
Juan Carrasco <jgcarrascob@ine.gob.cl>
2023-10-22 19:20:18 -0300

    data

--- data-raw/accident-2013.R
+++ data-raw/accident-2013.R
@@ -1,4 +1,5 @@
 ## code to prepare `accident 2013` dataset goes here
 
 accident_2013  <- read.csv("C:/Users/juan_/Downloads/data/data/accident_2013.csv")[1:10,1:3]
-usethis::use_data(accident_2013, overwrite = TRUE)
+usethis::use_data(accident_2013, overwrite = TRUE, internal = TRUE)
+
